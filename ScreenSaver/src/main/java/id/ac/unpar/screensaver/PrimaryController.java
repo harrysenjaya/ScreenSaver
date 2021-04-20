948a092d6eedd0fbd749da006b7f616fc239544b
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2021-04-20 09:52:38 +0700

    Mengganti image loading dengan JavaFX tanpa melibatkan Swing

--- ScreenSaver/src/main/java/id/ac/unpar/screensaver/PrimaryController.java
+++ ScreenSaver/src/main/java/id/ac/unpar/screensaver/PrimaryController.java
@@ -3,12 +3,9 @@ package id.ac.unpar.screensaver;
 import id.ac.unpar.screensaver.studentportal.Scraper;
 import id.ac.unpar.screensaver.studentportal.StudentPortalDataPuller;
 import id.ac.unpar.siamodels.Mahasiswa;
-import id.ac.unpar.siamodels.TahunSemester;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import javafx.scene.image.Image;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.Period;
@@ -17,17 +14,12 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
-import javafx.animation.PauseTransition;
 import javafx.animation.Timeline;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
-import javax.imageio.ImageIO;
-import sun.misc.BASE64Decoder;
 
 public class PrimaryController implements Initializable {
 
@@ -58,12 +50,8 @@ public class PrimaryController implements Initializable {
             StudentPortalDataPuller puller = new StudentPortalDataPuller();
             Mahasiswa[] listMahasiswa = puller.pullMahasiswas();
             listMahasiswa[0] = puller.pullMahasiswaDetail();
-//            InputStream is = new ByteArrayInputStream(listMahasiswa[0].getPhotoImage());
-//            BufferedImage newBi = ImageIO.read(is);
-            byte[] fotoByte = listMahasiswa[0].getPhotoImage();
-            ByteArrayInputStream bais = new ByteArrayInputStream(fotoByte);
-            BufferedImage bi = ImageIO.read(bais);
-            WritableImage image = SwingFXUtils.toFXImage(bi, null);
+            ByteArrayInputStream bais = new ByteArrayInputStream(listMahasiswa[0].getPhotoImage());
+			Image image = new Image(bais);
             this.foto.setImage(image);
             this.foto.fitWidthProperty();
             this.foto.fitHeightProperty();
@@ -72,7 +60,7 @@ public class PrimaryController implements Initializable {
             this.usia.setText(Period.between(listMahasiswa[0].getTanggalLahir(), LocalDate.now()).getYears() + " tahun " + Period.between(listMahasiswa[0].getTanggalLahir(), LocalDate.now()).getMonths() + " bulan " + " (lahir " + listMahasiswa[0].getTanggalLahir().toString() + ")");
             this.status.setText("Tidak Tersedia");
             this.email.setText(listMahasiswa[0].getEmailAddress());
-            if (listMahasiswa[0].getNilaiTOEFL().size() > 0) {
+            if (listMahasiswa[0].getNilaiTOEFL() != null && listMahasiswa[0].getNilaiTOEFL().size() > 0) {
                 this.toefl.setText(listMahasiswa[0].getNilaiTOEFL().get(listMahasiswa[0].getNilaiTOEFL().firstKey()).toString());
             } else {
                 this.toefl.setText("Tidak Tersedia");
@@ -106,8 +94,6 @@ public class PrimaryController implements Initializable {
         } catch (IOException ex) {
             Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IllegalStateException ex) {
-            Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InterruptedException ex) {
             Logger.getLogger(PrimaryController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
